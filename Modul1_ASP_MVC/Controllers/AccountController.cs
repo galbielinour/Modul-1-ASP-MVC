a8fcd299e9157a0bc70aba690b21941075730dc7
GABS <galbie_elinour_24rpl@student.smktelkom-mlg.sch.id>
2017-01-24 15:10:38 +0700

    Add project files.

---
+++ Modul1_ASP_MVC/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using Modul1_ASP_MVC.Models.ViewModel;
+using Modul1_ASP_MVC.Models.EntityManager;
+
+namespace Modul1_ASP_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
---
+++ Modul1_ASP_MVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Modul1_ASP_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
